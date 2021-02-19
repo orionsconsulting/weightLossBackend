fcb0b87b7099fb2e7cf07e5c6df3a4657094b0c5
Thomas Williams <orion5williams@gmail.com>
2021-02-19 10:00:29 -0600

    changes related to oauth :

--- weightLoss/weightLoss/serializers.py
+++ weightLoss/weightLoss/serializers.py
@@ -6,13 +6,17 @@ class UserSerializer(serializers.Serializer):
     pk = serializers.IntegerField(read_only=True)
     firstName = serializers.CharField(max_length=30)
     lastName = serializers.CharField(max_length=30)
+    username = serializers.CharField(max_length=30)
+    password = serializers.CharField(max_length=30)
 
     def create(self, validated_data):
         return User.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.firstName = validated_data.get('name', instance.firstName)
-        instance.lastName = validated_data.get('description', instance.lastName)
+        instance.firstName = validated_data.get('firstName', instance.firstName)
+        instance.lastName = validated_data.get('lastName', instance.lastName)
+        instance.username = validated_data.get('username', instance.username)
+        instance.password = validated_data.get('password', instance.password)
         instance.save()
         return instance
 
